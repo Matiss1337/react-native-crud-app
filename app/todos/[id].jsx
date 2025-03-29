4331dbb029ab0df221087c00362307bae943489b
Matiss Judins <matissj1337@gmail.com>
2025-03-29 17:02:28 +0200

    progress, need break

--- app/todos/[id].jsx
+++ app/todos/[id].jsx
@@ -1,8 +1,45 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Pressable, TextInput } from 'react-native';
+import { useState, useEffect, useContext } from 'react';
 import { useLocalSearchParams } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ThemeContext } from '@/context/ThemeContext';
+import { StatusBar } from 'react-native';
+import { Inter_500Medium, useFonts } from '@expo-google-fonts/inter';
+import {Octicons} from "@expo/vector-icons/Octicons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useRouter } from "expo-router";
 
 export default function TodoScreen() {
     const { id } = useLocalSearchParams();
+    const [toDo, setToDo] = useState({});
+    const { colorScheme, setColorScheme, theme } = useContext(ThemeContext)
+    const router = useRouter();
+
+    const [loaded, error] = useFonts({
+        Inter_500Medium,
+    })
+
+    useEffect(() => {
+        const fetchData = async (id) => {
+            try {
+                const jsonValue = await AsyncStorage.getItem("TodoApp")
+                const storageTodos = jsonValue != null ? JSON.parse(jsonValue) : null
+
+                if (storageTodos && storageTodos.length) {
+                    const myTodo = storageTodos.find(todo => todo.id.toString() === id)
+                    setToDo(myTodo)
+                }
+            } catch {
+                console.error(e)
+            }
+        }
+    }, [])
+    // check localStorage and filters by ID then set itt in state so we can operate
+
+    if (!loaded && !error) {
+        return null
+    }
+
 
     return (
         <View>
